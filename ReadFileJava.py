cfcf563feb2190cc41c33fa4d58fcca075e7ddc8
nurfauzanikmi <nurfauzanikmi91@live.com>
2015-10-03 08:38:45 +0800

    Python Version

---
+++ ReadFileJava.py
@@ -0,0 +1,14 @@
+Python 2.7.10 (default, May 23 2015, 09:40:32) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> file = open('Nur Fauzan Ikmi.txt', 'r')
+>>> print file.read()
+
+>>> file = open('C:/Users/Fauzan/Desktop/Nur Fauzan Ikmi.txt', 'r')
+>>> print file.read()
+Name	:Nur Fauzan Ikmi
+Matrix No	:2014244296
+Gender	:Female
+Age	:24
+Emails	:nurfauzanikmi91@live.com
+Mobile Phone	:0134072162
+>>> 
---
+++ WriteFileJava.py
@@ -0,0 +1,11 @@
+Python 2.7.10 (default, May 23 2015, 09:40:32) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> file = open("C:/Users/Fauzan/Desktop/Nur Fauzan Ikmi.txt", "w")
+>>> lines_of_text = ["Name:Nur Fauzan Ikmi", "Matrix No: 2014244296", "Gender: Female", "Age:24","E-mails:nurfauzanikmi91@live.com","Mobile Phone:01340472162"]
+>>> file.writelines(lines_of_text)
+>>> file.close()
+>>> file = open("C:/Users/Fauzan/Desktop/Nur Fauzan Ikmi.txt", "w")
+>>> lines_of_text = ["Name:Nur Fauzan Ikmi", "Matrix No: 2014244296", "Gender: Female", "Age:24","E-mails:nurfauzanikmi91@live.com","Mobile Phone:01340472162"]
+>>> file.writelines(lines_of_text)
+>>> file.close()
+>>> 
